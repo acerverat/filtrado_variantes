34825101d021700c3ce830adad8b75f3d819dc5e
Schamane <berith6041@ciencias.unam.mx>
2022-05-12 04:45:37 -0500

    file check

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # https://cancer.sanger.ac.uk/cosmic/register
 # Please replace email and pass variables with your own credentials.
-email=berith6041@ciencias.unam.mx
-pass=Cosmic718281.
+email=CORREO
+pass=CONTRASENA
 auth=$(echo "$email:$pass" | base64)
 jp2a public/welcome.jpg
 sleep 2
@@ -15,24 +15,31 @@ cd ..
 sudo apt-get install python3-pip curl jp2a jq python3-tk
 pip install autopep8 virtualenv python-decouple tk
 
-# Authentication to cosmos database.
-url=$(curl -H "Authorization: Basic $auth" https://cancer.sanger.ac.uk/cosmic/file_download/GRCh38/cosmic/v95/CosmicMutantExport.tsv.gz | jq -r '.url')
-wait
-jp2a public/chromosome.jpg
-echo "Downloading variants from cosmos database..."
+# Getting cosmic variants.
 mkdir data
 cd data
-# curl "$url" --output cosmic_variants.tsv
-
 if [ -e cosmic_variants.tsv ]; then
-    jp2a ../public/INMEGEN_logo.jpg
-    echo "The file cosmic_variants.tsv was correctly loaded you can now run the tests of the program using ./test"
-    sleep 5
+    echo "cosmic_variants.tsv already exist"
+    sleep 1
     cd ../views
     python3 interface.py
 else
-    echo "The file cosmic_variants.tsv was not loaded correctly"
+    url=$(curl -H "Authorization: Basic $auth" https://cancer.sanger.ac.uk/cosmic/file_download/GRCh38/cosmic/v95/CosmicMutantExport.tsv.gz | jq -r '.url')
+    wait
+    jp2a ../public/chromosome.jpg
+    echo "Downloading variants from cosmos database..."
+    curl "$url" --output cosmic_variants.tsv
+    if [ -e cosmic_variants.tsv ]; then
+        jp2a ../public/inmegen_logo.jpg
+        echo "The file cosmic_variants.tsv was correctly loaded you can now run the tests of the program using ./test"
+        sleep 3
+        cd ../views
+        python3 interface.py
+    else
+        echo "The file cosmic_variants.tsv was not loaded correctly"
+    fi
 fi
+
 cd ..
 
 # For more info on extracting the cosmos database look at:
